3ff9e2b21324eaa6540c42a1d1eba92e45e75a84
leomouraire1 <leomouraire1@gmail.com>
2019-08-24 18:22:48 +0200

    setup home static page

--- src/javascript/components/CanvasComponent.js
+++ src/javascript/components/CanvasComponent.js
@@ -19,26 +19,57 @@ class CanvasComponent {
     this._canvas = document.querySelector('.js-canvas-component');
     this._ctx = this._canvas.getContext('2d');
 
+    this.ui = {
+        section: document.querySelector('.js-section-canvas')
+    }
+
     this._settings = {
       scrollVelocityFactor: 0.5,
       marginFactor: 0.5
     }
 
+    this._tweenObject = {
+        currentIndex: 0,
+        index: 0
+    }
+
+    this._colors = [
+        {
+            primary: '#faf3e1',
+            secondary: '#121212'
+        },
+        {
+            primary: '',
+            secondary: ''
+        },
+        {
+            primary: '',
+            secondary: ''
+        },
+        {
+            primary: '',
+            secondary: ''
+        }
+    ];
+
     this._stats = new Stats();
     this._stats.showPanel(0);
-    document.body.appendChild(this._stats.dom);
+    // document.body.appendChild(this._stats.dom);
 
     this._init();
   }
 
   _init() {
     this._resize();
+    this._getSectionPosition();
+    this._setColor();
 
     this._loadImages();
   }
-  
-  _start() {
-    this._setupEventListener();
+
+  _setColor() {
+      this._canvas.style.backgroundColor = this._colors[this._tweenObject.index].primary;
+      document.body.style.color = this._colors[this._tweenObject.index].secondary;
   }
 
   _loadImages() {
@@ -65,12 +96,28 @@ class CanvasComponent {
     });
   }
 
+  _start() {
+    this._setupEventListener();
+  }
+
   _scrollManager(e) {
       this._scrollVelocity = e.deltaY * this._settings.scrollVelocityFactor;
   }
 
   _draw() {
     this._ctx.clearRect(0, 0, this._width, this._height);
+
+    this._createImages();
+  }
+
+  _createImages() {
+      const width = this._width/2.9;
+      const left = this._container.right - width - this._getNumber(this._container.padding);
+      const top = this._container.top;
+      const aspectRatio = this._images[0].width / this._images[0].height;
+      const height = width / aspectRatio; 
+
+      this._ctx.drawImage(this._images[0], left, top, width, height);
   }
 
   _resize() {
@@ -79,6 +126,20 @@ class CanvasComponent {
 
     this._canvas.width = this._width;
     this._canvas.height = this._height;
+  }
+
+  _getSectionPosition() {
+    this._container = {
+        left: this.ui.section.getBoundingClientRect().left,
+        top: this.ui.section.getBoundingClientRect().top,
+        right: this.ui.section.getBoundingClientRect().right,
+        bottom: this.ui.section.getBoundingClientRect().bottom,
+        padding: window.getComputedStyle(this.ui.section).paddingLeft
+    }
+  }
+
+  _getNumber(string) {
+    return parseInt(string.substring(0, string.length - 2))
   }
 
   _setupEventListener() {
@@ -101,6 +162,7 @@ class CanvasComponent {
 
   _resizeHandler() {
     this._resize();
+    this._getSectionPosition();
   }
 
   _wheelHandler(e) {
