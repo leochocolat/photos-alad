1913e7aa8e4bbfc6afd721892613c2bf160a0a9c
leomouraire1 <leomouraire1@gmail.com>
2019-08-26 17:07:43 +0200

    scroll indicator canvas

--- src/javascript/components/ScrollCirlceComponent.js
+++ src/javascript/components/ScrollCirlceComponent.js
@@ -6,10 +6,87 @@ import Lerp from '../utils/Lerp.js';
 class ScrollCircleComponent {
 
   constructor() {
+
+    _.bindAll(
+      this,
+      '_tickHandler'
+    );
   
+    this.ui = {
+      scollIndicator: document.querySelector('.js-scroll-indicator'),
+      canvas: document.querySelector('.js-scroll-indicator-fill')
+    }
+
+    this._canvas = this.ui.canvas;
+    this._ctx = this._canvas.getContext('2d');
+
+    this._delta = 0;
+
+    this._setup();
 
 
+  }
 
+  _setup() {
+    this._getStyle();
+    this._resize();
+    this._setupEventListeners();
+  }
+
+  _getStyle() {
+    this._canvasPosition = {
+      top: this.ui.canvas.getBoundingClientRect().top,
+      left: this.ui.canvas.getBoundingClientRect().left,
+    }
+  }
+
+  _resize() {
+    this._width = 20;
+    this._height = 20;
+
+    this._cirlceWidth = 15;
+    this._cirlceHeight = 15;
+
+    this._canvas.width = this._width;
+    this._canvas.height = this._height;
+    
+  }
+
+  _drawArc() {
+    this._ctx.strokeStyle = 'rgba(0, 0, 0, .2)';
+
+    this._ctx.beginPath();
+    this._ctx.arc(this._width/2, this._height/2, this._cirlceWidth/2, 0, 2 * Math.PI);
+    this._ctx.stroke();
+    this._ctx.closePath();
+  }
+  
+  _drawProgress() {  
+    this._ctx.strokeStyle = 'black';
+  
+    this._ctx.beginPath();
+
+    this._ctx.arc(this._width/2, this._height/2, this._cirlceWidth/2, 0, 2 * Math.PI * this._cirlceProgress);
+    this._ctx.stroke();
+
+    this._ctx.closePath();
+  }
+
+  _tick() {
+    this._ctx.clearRect(0, 0, this._width, this._height);
+
+    this._delta += 0.01;
+    this._cirlceProgress = Math.abs(Math.cos(this._delta));
+    this._drawArc();
+    this._drawProgress();
+  }
+
+  _setupEventListeners() {
+    TweenLite.ticker.addEventListener('tick', this._tickHandler);
+  }
+
+  _tickHandler() {
+    this._tick();
   }
 
 
